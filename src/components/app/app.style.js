fe1229f55c835065772bf0b237dccfdf722c8f31
Li Zheng <flyskywhy@gmail.com>
2021-12-17 16:52:47 +0800

    react -> react-native: just more flex like style

--- src/components/app/app.style.js
+++ src/components/app/app.style.js
@@ -3,15 +3,16 @@ import {colors} from '../../styles/variables.js';
 
 export default StyleSheet.create({
   app: {
-    height: '100%',
-    width: '100%',
-    display: 'flex',
+    flex: 1,
+    // width: '100%',
+    // height: '100%',
     flexDirection: 'row',
     backgroundColor: colors.paleblue,
   },
   appContent: {
     flexGrow: 1,
-    height: '100%',
+    // width: '100%',
+    // height: '100%',
     position: 'relative',
   },
 });
